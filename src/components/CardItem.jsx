91836ecdeb1a2c969f55732742fb5ada93c97b1e
Yi Yang <yiyang@MacBook-Pro.local>
2023-02-26 21:46:36 -0500

    fixed bug when transfer items on the card the previous functionality failed

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,9 +1,74 @@
 import { Card, Typography } from "@mui/material";
-import React from "react";
+import { React, useContext } from "react";
+import { ItemsContext } from "../itemContext/ItemsContexts";
 
 const CardItem = ({ item, category }) => {
+  const {
+    todo,
+    setTodo,
+    progress,
+    setProgress,
+    blocked,
+    setBlocked,
+    completed,
+    setCompleted,
+  } = useContext(ItemsContext);
   const handleOnDrag = (e) => {
     e.dataTransfer.setData("temp", JSON.stringify([category, item]));
+  };
+  const handleOnDrop = async (e) => {
+    let tempData = JSON.parse(e.dataTransfer.getData("temp"));
+    let categoryToRemove = tempData[0];
+    let idToRemove = tempData[1][0];
+    let item = tempData[1][1];
+    if (category !== categoryToRemove) {
+      console.log("dddd");
+      switch (category) {
+        case "todo":
+          setTodo([...todo, { id: idToRemove, item }]);
+          break;
+        case "progress":
+          setProgress([...progress, { id: idToRemove, item }]);
+          break;
+        case "blocked":
+          setBlocked([...blocked, { id: idToRemove, item }]);
+          break;
+        case "completed":
+          setCompleted([...completed, { id: idToRemove, item }]);
+          break;
+        default:
+      }
+    }
+
+    let tempArr = {};
+    let newArr = {};
+
+    if (category !== categoryToRemove) {
+      console.log("dddd");
+      switch (categoryToRemove) {
+        case "todo":
+          tempArr = todo;
+          newArr = tempArr.filter((item) => item.id !== idToRemove);
+          setTodo(newArr);
+          break;
+        case "progress":
+          tempArr = progress;
+          newArr = tempArr.filter((item) => item.id !== idToRemove);
+          setProgress(newArr);
+          break;
+        case "blocked":
+          tempArr = blocked;
+          newArr = tempArr.filter((item) => item.id !== idToRemove);
+          setBlocked(newArr);
+          break;
+        case "completed":
+          tempArr = completed;
+          newArr = tempArr.filter((item) => item.id !== idToRemove);
+          setCompleted(newArr);
+          break;
+        default:
+      }
+    }
   };
   return (
     <Card
@@ -17,6 +82,10 @@ const CardItem = ({ item, category }) => {
       }}
       draggable
       onDragStart={handleOnDrag}
+      onDrop={handleOnDrop}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
     >
       <Typography sx={{ justifyContent: "center" }}>{item[1]}</Typography>
     </Card>
