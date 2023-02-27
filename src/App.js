61a53d440257de6877199eec9dca7186b10785f8
Yi Yang <yiyang@MacBook-Pro.local>
2023-02-26 20:29:01 -0500

    set up cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import "./App.css";
-
+import "./App.scss";
+import { Typography, Box, Card, Chip } from "@mui/material";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Typography variant="h3" mt={20}>
+        Kanban Board
+      </Typography>
+      <Box
+        sx={{
+          width: "80%",
+          height: "70%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+        mt={5}
+      >
+        <Card sx={{ width: "20%", height: "80%" }}>
+          <Chip
+            label="Todo"
+            color="primary"
+            sx={{ fontSize: "20px", padding: "10px", margin: "15px 0" }}
+          />
+        </Card>
+        <Card sx={{ width: "20%", height: "80%" }}>
+          <Chip
+            label="In Progress"
+            color="secondary"
+            sx={{ fontSize: "20px", padding: "10px", margin: "15px 0" }}
+          />
+        </Card>
+        <Card sx={{ width: "20%", height: "80%" }}>
+          <Chip
+            label="Blocked"
+            color="warning"
+            sx={{ fontSize: "20px", padding: "10px", margin: "15px 0" }}
+          />
+        </Card>
+        <Card sx={{ width: "20%", height: "80%" }}>
+          <Chip
+            label="Completed"
+            color="success"
+            sx={{ fontSize: "20px", padding: "10px", margin: "15px 0" }}
+          />
+        </Card>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
